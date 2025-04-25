3a996c0f9c4fe385e57bc07f0bd2b7319cc2dff0
Tomas D. <32208899+Tomas-Duron@users.noreply.github.com>
2025-04-24 21:09:45 -0700

    Changed to allow flags
    Moved around the formatting of the file to allow for the usage of flags.

--- runner.sh
+++ runner.sh
@@ -4,19 +4,21 @@
 # If you're running this in a linux environment you need to replace both of the "./a.exe" with "./a.out"
 
 if [ -z $1 ] || [ -z $2 ]; then
-    echo "[compiler] [directory & filename]";
+    echo "[directory & filename] [action] [flags]";
 else
-    COMPILER="$1";
-    FILEPATH="$2";
+    FILEPATH="$1";
+    ACTION="$2";
+    FLAGS="$3"
 
     if [ -f "$FILEPATH" ]; then
         FILENAME="${FILEPATH##*/}";
         DIRECTORY="${FILEPATH%/*}";
 
         if [ "$DIRECTORY" == "$FILENAME" ]; then
-            $COMPILER $FILENAME && ./a.exe;
+            $ACTION $FILENAME && ./a.out;
         else
-            cd $DIRECTORY && $COMPILER $FILENAME && ./a.exe;
+            cd $DIRECTORY && $ACTION $FILENAME $FLAGS && ./a.out;
         fi
+        echo -e;
     fi
 fi
